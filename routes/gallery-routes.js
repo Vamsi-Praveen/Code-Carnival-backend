2612da148ad3ac6e875eff016015cb847fdbba0c
Vamsi-Praveen <21A91A05G5@aec.edu.in>
2023-11-12 17:17:14 +0530

    added js

--- routes/gallery-routes.js
+++ routes/gallery-routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getGallery,insertGallery } from "../controllers/gallery-controller";
+import { getGallery,insertGallery } from "../controllers/gallery-controller.js";
 
 const galleryrRouter = express.Router();
 
